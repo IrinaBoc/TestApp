66f57f0e03f6ef97a68b39d1ce127293bec1801b
Irina <irinacoding@gmail.com>
2024-10-22 23:08:51 +0300

    Добавьте файлы проекта.

---
+++ TestApp/SomeSource.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestApp
+{
+    class SomeSource:ViewModelBase
+    {       
+        private string _inputText;
+        private bool _enabled;
+
+        public SomeSource()
+        {
+            PopUpEnabled = true;
+        }
+        public bool TextInputDone {get; set; }
+        public bool PopUpEnabled
+        {
+            get { return _enabled; }
+            set
+            {
+                _enabled = value;
+                OnPropertyChanged();
+            }
+        }
+        public string InputIext
+        {
+            get { return _inputText; }
+            set
+            {
+                _inputText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GetUserInputCommand
+        {
+            get
+            {
+                return new RelayCommand( (obj) =>
+                {
+                    TextInputDone = true;
+                });
+            }
+        }
+        internal string GetData()
+        {
+            try
+            {
+                while (!TextInputDone)
+                {
+                    
+                }
+                PopUpEnabled = false;
+                return InputIext;
+            }
+            catch (Exception)
+            {
+                PopUpEnabled = false;               
+            }
+            return null;
+        }
+    }
+}
---
+++ TestApp/SomeViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestApp
+{
+    class SomeViewModel: ViewModelBase
+    {
+        
+        private SomeSource _context;
+
+        public string Data { get; set; }
+       
+        public SomeSource PopUpContext
+        {
+            get { return _context; }
+            set
+            {
+                _context = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GetDataCommand
+        {
+            get
+            {
+                return new RelayCommand(async (obj) =>
+                {                   
+                    var ds = new SomeSource();
+                    PopUpContext = ds;
+                    Data = await Task.Run(() => ds.GetData());
+                });
+            }
+        }
+
+    
+    }
+}
